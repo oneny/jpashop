13c2207900ee36f9633157bdf0fa80fb7451b70e
oneny <gjwodnjs1996@gmail.com>
2023-06-10 14:59:29 +0900

    add: complete "회원 도메인 개발"

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,53 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+  @Autowired
+  MemberService memberService;
+  @Autowired
+  MemberRepository memberRepository;
+
+  @Test
+  public void 회원가입() {
+
+    // given
+    Member member = new Member();
+    member.setName("kim");
+
+    // when
+    Long saveId = memberService.join(member);
+
+    // then
+    assertThat(member).isEqualTo(memberRepository.findOne(saveId));
+  }
+
+  @Test
+  public void 중복_회원_예외() {
+    // given
+    Member member1 = new Member();
+    member1.setName("kim");
+
+    Member member2 = new Member();
+    member2.setName("kim");
+
+    // when
+    memberService.join(member1);
+
+    // then
+    assertThatThrownBy(() -> memberService.join(member2))
+            .isInstanceOf(IllegalStateException.class)
+            .hasMessage("이미 존재하는 회원입니다");
+  }
+}
